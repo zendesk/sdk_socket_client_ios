612e6ccd8c12615162806427663d1ff90b790f50
runner <runner@zendesk.com>
2023-10-03 08:28:40 +0000

    [RELEASE] New version 1.6.0

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "ZendeskSDKSocketClient",
     platforms: [
-        .iOS(.v11)
+        .iOS(.v12)
     ],
     products: [
         .library(
@@ -14,20 +14,24 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(name: "ZendeskSDKLogger",
-                 url: "https://github.com/zendesk/sdk_logger_ios",
-                 from: "0.8.0")
+        .package(
+            name: "ZendeskSDKLogger",
+            url: "https://github.com/zendesk/sdk_logger_ios",
+            from: "0.9.0"
+        )
     ],
     targets: [
         .binaryTarget(
             name: "ZendeskSDKSocketClient",
             path: "ZendeskSDKSocketClient.xcframework"
         ),
-        .target(name: "ZendeskSDKSocketClientTargets",
-                dependencies: [
-                    .target(name: "ZendeskSDKSocketClient"),
-                    .product(name: "ZendeskSDKLogger", package: "ZendeskSDKLogger")
-                ],
-                path: "Sources")
+        .target(
+            name: "ZendeskSDKSocketClientTargets",
+            dependencies: [
+                .target(name: "ZendeskSDKSocketClient"),
+                .product(name: "ZendeskSDKLogger", package: "ZendeskSDKLogger")
+            ],
+            path: "Sources"
+        )
     ]
 )
--- ZendeskSDKSocketClient.podspec
+++ ZendeskSDKSocketClient.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
   s.name         = "ZendeskSDKSocketClient"
-  s.version      = "1.5.0"
+  s.version      = "1.6.0"
   s.summary      = "ZendeskSDKSocketClient #{s.version.to_s}"
   s.homepage     = "https://developer.zendesk.com/documentation/zendesk-web-widget-sdks/sdks/ios/getting_started/"
   s.license      = {
@@ -16,11 +16,11 @@ Pod::Spec.new do |s|
     LICENSE
   }
   s.author       = 'Zendesk'
-  s.platform     = :ios, '11.0'
+  s.platform     = :ios, '12.0'
   s.requires_arc = true
   s.swift_version = '5.3'
   s.cocoapods_version = '>= 1.10.0'
   s.source       = { :git => "https://github.com/zendesk/sdk_socket_client_ios.git", :tag => s.version }
   s.vendored_frameworks = 'ZendeskSDKSocketClient.xcframework'
-  s.dependency 'ZendeskSDKLogger', '~> 0.8.0'
+  s.dependency 'ZendeskSDKLogger', '~> 0.9.0'
 end
